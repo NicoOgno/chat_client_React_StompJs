16662ad0c2fabc76d73f79562d97c6f3be26faa2
NicoOgno <blucode.sur@gmail.com>
2023-11-09 11:38:08 +0100

    Basic connection to server

--- client/src/components/ChatRoom/ChatRoom.jsx
+++ client/src/components/ChatRoom/ChatRoom.jsx
@@ -2,8 +2,35 @@ import styles from './chatRoom.module.css'
 
 function ChatRoom() {
   
+    const stomp = new Client({
+        brokerURL: 'ws://localhost:8080/chat-websocket-service',
+      });
+  
+      stomp.onConnect = (frame) => {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stomp.subscribe('/topic/' + room, (message) => {
+          showMessage(JSON.parse(message.body).content);
+        });
+      };
+  
+      stomp.onWebSocketError = (error) => {
+        console.error('Error with websocket', error);
+      };
+  
+      stomp.onStompError = (frame) => {
+        console.error('Broker reported error: ' + frame.headers.message);
+        console.error('Additional details: ' + frame.body);
+      };
 
-
+      const connect = () => {
+        stompClient.activate();
+      };
+    
+      const disconnect = () => {
+        stompClient.deactivate();
+        console.log('Disconnected');
+      };
 
   return (
     <div className={styles.chatContainer}>
@@ -11,10 +38,10 @@ function ChatRoom() {
       <div className={styles.chatBox}>
         <h2>Messages</h2>
       </div>
-        <button>
+        <button onClick={connect}>
           Connect
         </button>
-        <button >
+        <button onClick={disconnect}>
           Disconnect
         </button>
         <button>
